4a29adb13c6b3eba1e3b6f389defa54e9f0ae5f5
InsZVA <inszva@126.com>
2016-06-01 23:41:41 +0800

    这语言表现力太弱了，垃圾，弃坑！

--- go2.py
+++ go2.py
@@ -1,5 +1,6 @@
 import multiprocessing
 import time
+import random
 
 class P:
     rgqueue = None
@@ -32,24 +33,96 @@ class P:
 
 class M:
     p = None
-    spinning = False
+    nmspinning = None
+    gfpqueue = None
+    gmlist = None
+    """
+        :type nmspinning: multiprocessing.Value
+    """
+    g = None
+    
+    def __init__(self, nmspinning, gfpqueue, gmlist):
+        print 'New M'
+        self.nmspinning = nmspinning
+        self.gfpqueue = gfpqueue
+        self.gmlist = gmlist
     
     def spin(self):
+        #with self.nmspinning.get_lock():
+        self.nmspinning.value = self.nmspinning.value + 1
+        race = 0
+        while race < 10000:
+            race = race + 1 
+        while self.nmspinning.value == 1:
+            1 == 1
+        #with self.nmspinning.get_lock():
+        self.nmspinning.value = self.nmspinning.value - 1
+        if self.gfpqueue.qsize() != 0:
+            p = self.gfpqueue.get()
+            self.p = p
+            while 1 == 1:
+                if p.rgqueue.empty():
+                    i = random.randint(0, len(self.gmlist) - 1)
+                    if p.stealq(self.gmlist[i].p):
+                        g = p.rgqueue.get()
+                        g['func'](g['args'])
+                        continue
+                    else:
+                        return
+                else:
+                    g = p.rgqueue.get()
+                    g['func'](g['args'])
+                    continue
+        else:
+            print 'No free P'
+            
         
+
+def gocommit(gplist, nmspinning, gmlist, gfpqueue, func, args):
+    """
+        :type nmspinning: multiprocessing.Value
+        :type gplist: multiprocessing.List(P)
+    """
+    i = random.randint(0, multiprocessing.cpu_count() - 1)
+    while gplist[i].m == None:
+        i = (i + 1) % multiprocessing.cpu_count()
+        #TODO: All P are block
+    gplist[i].rgqueue.put({'func': func, 'args': args})
+    if nmspinning.value > 0 and len(gmlist) < multiprocessing.cpu_count():
+        m = M(nmspinning, gfpqueue, gmlist)
+        gmlist.append(m)
+        p = multiprocessing.Process(target=m.spin)
+        p.start()
+
+def test(msg):
+    print msg
+    time.sleep(10)
 
 if __name__ == '__main__':
     MAX_PROCS = multiprocessing.cpu_count()
-    
+    mgr = multiprocessing.Manager();
+    gfpqueue = mgr.Queue(0)
+    gplist = mgr.list()
+    gmlist = mgr.list()
+    grgqueue = mgr.Queue(0)
     i = 0
-    rgqueues = []
     while i < MAX_PROCS:
-        q = multiprocessing.Queue(0)
-        rgqueues.append(q)
+        q = mgr.Queue(0)
+        p = P(q)
+        gplist.append(p)
+        gfpqueue.put(p)
         i = i + 1
     
-    grgqueue = multiprocessing.Queue(0)
-    gfpqueue = multiprocessing.Queue(0)
-    gfmqueue = multiprocessing.Queue(0)
-    gplist = multiprocessing.List()
-    nmspinning = multiprocessing.Value('d', 0)
+    nmspinning = mgr.Value('i', 0)
+    m = M(nmspinning, gfpqueue, gmlist)
+    gmlist.append(m)
+    p = multiprocessing.Process(target=m.spin)
+    p.start()
+    
+    gocommit(gplist, nmspinning, gmlist, test, 1)
+    gocommit(gplist, nmspinning, gmlist, test, 2)
+    gocommit(gplist, nmspinning, gmlist, test, 3)
+    gocommit(gplist, nmspinning, gmlist, test, 4)
+    gocommit(gplist, nmspinning, gmlist, test, 5)
+    gocommit(gplist, nmspinning, gmlist, test, 6)
     
